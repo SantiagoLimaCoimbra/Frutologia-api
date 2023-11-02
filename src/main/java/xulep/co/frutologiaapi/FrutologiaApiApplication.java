c9f5c79c24a5f972351189c76b6d4f51950fcf44
SantiagoLimaCoimbra <santiagolimacoimbra@gmail.com>
2023-11-02 19:28:56 -0300

    Base do projeto

---
+++ src/main/java/xulep/co/frutologiaapi/FrutologiaApiApplication.java
@@ -0,0 +1,13 @@
+package xulep.co.frutologiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrutologiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrutologiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xulep/co/frutologiaapi/FrutologiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xulep.co.frutologiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrutologiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
